57a50be3bf002f17118b7206018b42c12383dc4a
Tharathip Tamwiset <70807925+fronceee@users.noreply.github.com>
2025-03-24 18:39:26 +0000

    1.7: unicafe step 2

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -5,10 +5,43 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [score, setScore] = useState(0)
+
+  const totalClicked = good + neutral + bad
+
+  const handleGood = () => {
+    setScore(prev => prev += 1)
+    setGood(prev => prev += 1)
+  }
+
+  const handleNeutral = () => {
+    setNeutral(prev => prev += 1)
+  }
+  
+  const handleBad = () => {
+    setScore(prev => prev -= 1)
+    setBad(prev => prev += 1)
+  }
+
+  const average = score / totalClicked
+
+  const positivePercent = (good / totalClicked) * 100
+
 
   return (
     <div>
-      code here
+      <h1>give feedback</h1>
+      <div>
+        <button onClick={handleGood}>good</button>
+        <button onClick={handleNeutral}>neutral</button>
+        <button onClick={handleBad}>bad</button>
+      </div>
+      <h1>statistics</h1>
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
+      <p>average {average}</p>
+      <p>positive {positivePercent} %</p>
     </div>
   )
 }
