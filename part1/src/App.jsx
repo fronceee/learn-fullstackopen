3767e97f24a17e0b14166007cd57c5ee7864a891
Tharathip Tamwiset <70807925+fronceee@users.noreply.github.com>
2025-03-24 18:13:25 +0000

    1.4 make Content component accept array

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -2,33 +2,36 @@ const Header = ({course}) => (<h1>{course}</h1>)
 
 const Part = ({part, exercises}) => (<p>{part} {exercises}</p>)
 
-const Content = ({part1, part2, part3}) => (<div>
-  <Part part={part1.name}  exercises={part1.exercises}/>
-  <Part part={part2.name}  exercises={part2.exercises}/>
-  <Part part={part3.name}  exercises={part3.exercises}/>
+const Content = ({parts}) => (<div>
+  <Part part={parts[0].name}  exercises={parts[0].exercises}/>
+  <Part part={parts[1].name}  exercises={parts[1].exercises}/>
+  <Part part={parts[2].name}  exercises={parts[2].exercises}/>
 </div>)
 
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = {
-    name: 'Fundamentals of React',
-    exercises: 10
-  }
-  const part2 = {
-    name: 'Using props to pass data',
-    exercises: 7
-  }
-  const part3 = {
-    name: 'State of a component',
-    exercises: 14
-  }
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ]
+
 
 
   return (
     <div>
       <Header course={course} />
-      <Content part1={part1} part2={part2} part3={part3} />
-      <p>Number of exercises {part1.exercises + part2.exercises + part3.exercises}</p>
+      <Content parts={parts} />
+      <p>Number of exercises {parts[0].exercises + parts[1].exercises + parts[2].exercises}</p>
     </div>
   )
 }
