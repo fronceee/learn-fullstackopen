6838f862d3f7043e24fe1ab9a39aebbd05c20dc0
Tharathip Tamwiset <70807925+fronceee@users.noreply.github.com>
2025-03-30 16:33:12 +0000

    2.8: The Phonebook Step 3

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -2,9 +2,10 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas', phone: '554-5556545' }
   ])
   const [newName, setNewName] = useState('')
+  const [newPhone, setNewPhone] = useState('')
 
   return (
     <div>
@@ -19,18 +20,22 @@ const App = () => {
           return
         }
 
-        setPersons(prev => [...prev, {name: newName}])
+        setPersons(prev => [...prev, {name: newName, phone: newPhone}])
         setNewName('')
+        setNewPhone('')
       }}>
         <div>
           name: <input value={newName} onChange={event => setNewName(event.target.value)} />
+        </div>
+        <div>
+          number: <input value={newPhone} onChange={event => setNewPhone(event.target.value)} />
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map(item => (<p key={item.name}>{item.name}</p>))}
+      {persons.map(item => (<p key={item.name}>{item.name} {item.phone}</p>))}
     </div>
   )
 }
