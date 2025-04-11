7ee120ec09ac34106f230634cce4818714026f9e
Tharathip Tamwiset <70807925+fronceee@users.noreply.github.com>
2025-04-11 04:07:08 +0000

    3.8: Phonebook backend step 8

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -3,7 +3,12 @@ const morgan = require("morgan");
 const app = express();
 
 app.use(express.json());
-app.use(morgan("tiny"));
+morgan.token("req-body", (req) => JSON.stringify(req.body));
+app.use(
+	morgan(
+		":method :url :status :res[content-length] - :response-time ms :req-body",
+	),
+);
 
 const PORT = 3001;
 
@@ -47,7 +52,6 @@ app.get("/api/persons/:id", (request, response) => {
 
 app.post("/api/persons", (request, response) => {
 	const body = request.body;
-	console.log(typeof body);
 	if (!Object.keys(body)?.length) {
 		response.status(400).json({ error: "request body is empty" });
 	}
